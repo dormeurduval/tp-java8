9682f063a8634ab33645c3b9f216d51b68c5db00
CHAFFARD Joris <chaffard.joris@outlook.fr>
2017-10-03 10:23:41 +0200

    fin fonction

--- src/test/java/java8/ex05/Function_05_Test.java
+++ src/test/java/java8/ex05/Function_05_Test.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * Exercice 5 - java.util.function.Consumer
@@ -15,15 +16,18 @@ public class Function_05_Test {
     //tag::functions[]
     // TODO compléter la fonction
     // TODO modifier le mot de passe en "secret"
-    Consumer<Person> changePasswordToSecret = null;
+    Consumer<Person> changePasswordToSecret = (p)->
+    p.setPassword("secret");
 
     // TODO compléter la fonction
     // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = null;
+    Consumer<Person> verifyAge = 
+    		(p) -> {assert p.getAge()>4;};
 
     // TODO compléter la fonction
     // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
-    Consumer<Person> verifyPassword = null;
+    Consumer<Person> verifyPassword = 
+    (p) -> {assert p.getPassword().equals("secret");};
     //end::functions[]
 
 
@@ -37,9 +41,8 @@ public class Function_05_Test {
         // TODO remplacer la boucle for par l'invocation de la méthode forEach
         // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
         // personList.forEach...
-        for(Person p : personList) {
-            verifyAge.accept(p);
-            verifyPassword.accept(p);
-        }
+        personList.stream().forEach(changePasswordToSecret);
+        
+        personList.stream().forEach(verifyAge.andThen(verifyPassword));
     }
 }
